aaf85f0ea0b164a83b5f5ebecf5f571b7eeb3e1f
hasita-art <73806182+hasita-art@users.noreply.github.com>
2022-06-19 20:58:06 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,11 +23,20 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
+function modelLoaded(){
+  console.log("Model Loaded!");
+}
 
 function draw(){
-
+  image(0,0,700,600)
  background(0); 
 
  fill("black");
